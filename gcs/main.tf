fa01a34ec5519d433ea2ec18bdbdef66e8be2b50
Justin Lee <justin.lee@armory.io>
2020-01-30 11:07:18 +0100

    chore(tf): Terraformer GCS demo

---
+++ gcs/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "gcs" {
+      bucket = "justin-terraformer-gcs"
+      prefix = "terraform-gcs-test"
+      credentials = "/home/spinnaker/credentials/gcloud.json"
+  }
+}
+
+provider "google" {
+  credentials = file("/home/spinnaker/credentials/gcloud.json")
+  project = "cloud-armory"
+  region = "us-central1"
+  zone = "us-central1-c"
+}
+
+resource "google_storage_bucket" "test" {
+    name = "armory-justin-${var.bucket_postfix}"
+    location = "US"
+}
+
+variable "bucket_postfix" {
+  default = "helloworld"
+}
+
+output "bucket" {
+    value = "${google_storage_bucket.test.name}"
+}
